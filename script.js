8c0bcfaf5f289481a86f9dc259459b7ed2f831d4
renghinoITS <ilreleonecdc@gmail.com>
2025-02-16 22:28:43 +0100

    widget changed

--- script.js
+++ script.js
@@ -30,5 +30,25 @@ function updateImage() {
 	}
 }
 
+function caricaWidget() {
+	var container = document.getElementById("widget-container"); // Un contenitore dove inserire il widget
+
+	// Verifica la larghezza della finestra
+	if (window.innerWidth <= 768) {
+		// Carica il widget per cellulare
+		container.innerHTML =
+			'<script src="https://billetto.it/widget.js"></script>' +
+			'<billetto-organiser-widget type="list" organiser="6095917" organization="billetto.it" lang="it" theme="dark" color="#face06" include-private></billetto-organiser-widget>';
+	} else {
+		// Carica il widget per desktop
+		container.innerHTML =
+			'<script src="https://billetto.it/widget.js"></script>' +
+			'<billetto-organiser-widget type="calendar" organiser="6095917" organization="billetto.it" lang="it" theme="dark" color="#face06" include-private></billetto-organiser-widget>';
+	}
+}
+
+window.onload = caricaWidget;
+window.onresize = caricaWidget;
+
 updateImage();
 window.addEventListener("resize", updateImage);
