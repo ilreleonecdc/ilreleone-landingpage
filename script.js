37ccee0e85e1dc36f3437d23057e2bffa7b232bc
renghinoITS <ilreleonecdc@gmail.com>
2025-02-19 20:15:39 +0100

    updated documents

--- script.js
+++ script.js
@@ -15,10 +15,33 @@ window.addEventListener("load", function () {
 			const descrizione = document.getElementById("descrizione");
 			setTimeout(() => {
 				descrizione.style.opacity = "1"; // Appare gradualmente
-				descrizione.style.transform = "translateX(-50px)"; // Sposta a sinistra
+				descrizione.style.transform = "translateX(-40px)"; // Sposta a sinistra
 			}, 1000); // Aspetta 1 secondo dopo l'immagine
 		}, 2000); // Attesa iniziale di 3 secondi
 	}
+
+	// Funzione per cambiare il testo del paragrafo sotto i 481px
+	function cambiaTesto() {
+		const paragrafo = document.getElementById("p1");
+		if (window.innerWidth < 481) {
+			paragrafo.innerHTML =
+				'<br />In questa sezione potrai prenotare i tuoi biglietti per lo spettacolo. Una volta cliccato il bottone qui sotto <b>"Acquista Biglietti"</b> segui questi semplici passaggi: <br />';
+		}
+		const titoloh4 = document.getElementById("titoloh4");
+		if (window.innerWidth < 429) {
+			titoloh4.innerHTML =
+				"TORNA SABATO 22 FEBBRAIO alle <br /> ORE 12:00!";
+		}
+		if (window.innerWidth < 337) {
+			titoloh4.style.fontSize = "15px";
+		}
+	}
+
+	// Esegui la funzione al caricamento della pagina
+	cambiaTesto();
+
+	// Aggiungi un listener per cambiare il testo al resize della finestra
+	window.addEventListener("resize", cambiaTesto);
 });
 
 updateImage();
